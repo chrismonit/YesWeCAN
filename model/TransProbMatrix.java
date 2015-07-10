621330f26a576d6827dd20c71d0779969a74c017
Chris Monit <c.monit.12@ucl.ac.uk>
2015-07-10 15:50:52 +0100

    added code which performs eigendecomposition and exponentiation etc of rate matrix, to give P_t, given Q and t

--- model/TransProbMatrix.java
+++ model/TransProbMatrix.java
@@ -1,7 +1,11 @@
 
 package yeswecan.model;
 import org.apache.commons.math3.linear.EigenDecomposition;
+import org.apache.commons.math3.linear.RealMatrix; // may not need this by the end, useful in development
+import yeswecan.utils.MatrixPrinter;
 
+import java.lang.Math;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 
 /**
  *
@@ -13,22 +17,54 @@ import org.apache.commons.math3.linear.EigenDecomposition;
  */
 public class TransProbMatrix {
     
+    
+    private EigenDecomposition decomp;
+            
     public TransProbMatrix(RateMatrix Q){
         // perform eigen decomposition
         
-        //EigenDecomposition eigen = new EigenDecomposition(Q);
+        this.decomp = new EigenDecomposition(Q);
+        
+        
+        //get rid of this later
+        RealMatrix diag = decomp.getD();
+        double[][] data = diag.getData();
+        MatrixPrinter.PrintMatrix(data, "diag");
+        
+    }
+    
+
+    
+    
+    
+    public RealMatrix getP(double t){
+        
+        //Yang p69
+        
+        // Q = V*D*(V^-1)
+        // P(t) = exp(Qt) = U * exp(Dt) * (V^-1) = U * diag{ exp(d_1*t), exp(d_2*t) ... } * (V^-1)
+        // where (V^-1) = V_transpose (I think: Yang uses ^-1 while apache commons docs refers to transpose)
+        
+        RealMatrix V = decomp.getV();
+        
+        double[][] diag = decomp.getD().getData();  
+        for (int i = 0; i < diag.length; i++) {
+            diag[i][i] = Math.exp( diag[i][i] * t );
+        }
+        RealMatrix transformD = new Array2DRowRealMatrix(diag);
+        
+        RealMatrix VT = decomp.getVT();
+        
+        RealMatrix P_t = V.multiply(transformD.multiply(VT));
+    
+    
+        return P_t;
         
     }
     
     
     
-    /*
-    public static double[][] getP(double t){
     
-        // return P(t)
-        
-    }
-    */
     
     public static void main(String[] args){
         System.out.println("Testing TransProbMatrix");
