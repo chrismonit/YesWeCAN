746f90967817ab043700e5809a8840cca351ff4e
Chris Monit <c.monit.12@ucl.ac.uk>
2015-07-23 18:46:25 +0100

    continued work on Analyse

--- Analyse.java
+++ Analyse.java
@@ -4,6 +4,8 @@ package yeswecan;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import pal.alignment.Alignment;
 import pal.alignment.AlignmentReaders;
@@ -11,7 +13,9 @@ import pal.alignment.SimpleAlignment;
 import pal.datatype.Nucleotides;
 import pal.tree.ReadTree;
 import pal.tree.Tree;
+import swmutsel.model.parameters.BaseFrequencies;
 import swmutsel.model.parameters.Parameter;
+import swmutsel.model.parameters.TsTvRatio;
 import yeswecan.cli.CommandArgs;
 import yeswecan.phylo.AdvancedAlignment;
 
@@ -39,7 +43,6 @@ public class Analyse {
 
     }// main
     
-    
     private AdvancedAlignment alignment;
     private Tree tree;
     
@@ -55,16 +58,18 @@ public class Analyse {
         }
         
         loadData(comArgs.alignment(), comArgs.tree());
-        
+        //TODO
+        // params = makeModel(...)
+        // fit(data, model)
     }
     
     //TODO make more sophistcated exceptions to help user find problems. Separate tree and alignment reading in 
     public void loadData(String alignmentPath, String treePath){
         try{
-            alignment = new AdvancedAlignment(
+            this.alignment = new AdvancedAlignment(
                                 new SimpleAlignment(
                                         AlignmentReaders.readFastaSequences(new FileReader(alignmentPath), new Nucleotides())));
-            tree = new ReadTree(treePath);
+            this.tree = new ReadTree(treePath);
         }
         catch(Exception e){
             System.out.println(Constants.ERROR_PREFIX + "Unable to load alignment or tree file(s)");
@@ -74,16 +79,21 @@ public class Analyse {
     }
     
     
-    
+    // only HKY at the moment
     public List<Parameter> makeModel(CommandArgs comArgs){
-        
-    
+        TsTvRatio kappa = new TsTvRatio(comArgs.kappa());
+        BaseFrequencies pi = new BaseFrequencies(comArgs.pi());
+
+        return Arrays.asList(kappa, pi);        
     }
     
-    
-    
+    //TODO
+    // start the optimisation
     public void fit(){
-        
+        // make new instance of function, which takes in the data
+        //use Optimise to fit the model to the data
+        // get the MLEs
+        // format in some way
     }
     
     
