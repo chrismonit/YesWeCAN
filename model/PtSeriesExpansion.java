d561ac21a8c1518e026417a204553d2581fe0775
Chris Monit <c.monit.12@ucl.ac.uk>
2015-07-26 11:24:11 +0100

    implemented series expansion method of computing P in PtSeriesExpansion. Not yet tested

--- model/PtSeriesExpansion.java
+++ model/PtSeriesExpansion.java
@@ -6,6 +6,10 @@
 
 package yeswecan.model;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+import yeswecan.utils.MatrixPrinter;
+
 /**
  *
  * @author Christopher Monit <c.monit.12@ucl.ac.uk>
@@ -13,6 +17,57 @@ package yeswecan.model;
 public class PtSeriesExpansion implements ProbMatrixGenerator {
     
     
+    private RateMatrix Q;
+    private int highestTerm;
+    
+    public PtSeriesExpansion(RateMatrix Q, int highestTerm){
+        this.Q = Q;
+        this.highestTerm = highestTerm;
+    }
+    
+    private static double factorial(int n){
+        double product = 1.0;
+        for (int i = n; i > 0 ; i--) {
+            product *= i;
+        }
+        return product;
+    }
+    
+    public RealMatrix getP(double t){
+        return seriesExpansion(this.Q, t, this.highestTerm);
+    }
+    
+    
+    private static RealMatrix seriesExpansion(RateMatrix Q, double t, int highestTerm){
+        
+        /*
+            exp(Qt) = I + Qt + ((Qt)^2)/2! + ((Qt)^3)/3! + ... + ((Qt)^n)/n!
+        */
+        
+        RealMatrix Qt = Q.scalarMultiply(t);
+        
+        // make identity matrix
+        double[][] identity = new double[Q.getRowDimension()][Q.getColumnDimension()];
+        for (int i = 0; i < Q.getRowDimension(); i++) {
+            identity[i][i] = 1.0;
+        }
+                
+        RealMatrix sum = new Array2DRowRealMatrix(identity); // sum == I
+        
+        sum = sum.add( Qt ); // sum == I + Qt
+        
+        for (int n = 2; n <= highestTerm; n++) { // sum = I + Qt + ((Qt)^2)/2! + ((Qt)^3)/3! + ... + ((Qt)^n)/n!
+                        
+            double[][] termData = Qt.power(n).scalarMultiply(1.0/factorial(n)).getData();
+                        
+            RealMatrix term = new Array2DRowRealMatrix(termData);
+            sum = sum.add(term);
+            
+        }// for
+        
+        return sum;
+        
+    }// seriesExpansion
     
     
     
