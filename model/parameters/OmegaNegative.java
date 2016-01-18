217db9e2511ff3dfd855145d13ecfa1ea5dc5eba
Chris Monit <c.monit.12@ucl.ac.uk>
2016-01-18 09:58:45 +0000

    written OmegaNegative and OmegaPositive classes, which descend from Arif's Omega class. They implement constraints for 0 < w_0 < 1 and w_1 > 1 respectively. Need to test still

---
+++ model/parameters/OmegaNegative.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package yeswecan.model.parameters;
+
+import swmutsel.model.parameters.Omega;
+
+/**
+ *
+ * @author Christopher Monit <c.monit.12@ucl.ac.uk>
+ */
+public class OmegaNegative extends Omega {
+    
+    public OmegaNegative(){
+        setArgument("-omegaNeg");
+    }
+    
+    public OmegaNegative(double omega) {
+        super(omega);
+        setArgument("-omegaNeg");
+    }
+    
+    // f(x) = \frac{1}{1+e^{-x}} (where x is the optimasble value and f(x) is the value used in the model (parameter space))
+    @Override
+    public void setOptimisable(double[] params){
+        set( 1.0 / (1.0+Math.exp(-params[0])) );
+    }
+    
+    // x = -\ln \frac{1}{f(x)-1} (where x is the optimisable value)
+    @Override
+    public double[] getOptimisable() {
+        return new double[]{ -Math.log( (1.0/get()) - 1.0 )};
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("OmegaNegative{omega=%.7f}", get());
+    }
+    
+}
---
+++ model/parameters/OmegaPositive.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package yeswecan.model.parameters;
+
+import swmutsel.model.parameters.Omega;
+
+/**
+ *
+ * @author Christopher Monit <c.monit.12@ucl.ac.uk>
+ */
+public class OmegaPositive extends Omega {
+    
+    public OmegaPositive(){
+        setArgument("-omega+");
+    }
+    
+    public OmegaPositive(double omega) {
+        super(omega);
+        setArgument("-omega+");
+    }
+    
+    @Override
+    public void setOptimisable(double[] params){
+        set(1.0 + Math.exp(params[0]));
+    }
+    
+    @Override
+    public double[] getOptimisable() {
+        return new double[]{Math.log( get() - 1.0)};
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("OmegaPositive{omega=%.7f}", get());
+    }
+    
+}
