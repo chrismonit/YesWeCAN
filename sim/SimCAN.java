f35f921fbfb1ac9f3389f999ae0e791021ef144b
Chris Monit <c.monit.12@ucl.ac.uk>
2016-02-09 19:07:27 +0000

    written class SimCAN. untested

---
+++ sim/SimCAN.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package yeswecan.sim;
+
+import java.util.Random;
+import pal.alignment.Alignment;
+import pal.alignment.AlignmentBuilder;
+import pal.alignment.ConcatenatedAlignment;
+import pal.datatype.Nucleotides;
+import pal.tree.Node;
+import pal.tree.Tree;
+import swmutsel.model.parameters.BaseFrequencies;
+import swmutsel.model.parameters.BranchScaling;
+import swmutsel.model.parameters.Omega;
+import yeswecan.Constants;
+import yeswecan.model.CodonAwareMatrix;
+import yeswecan.model.ProbMatrixFactory;
+import yeswecan.model.ProbMatrixGenerator;
+import yeswecan.model.RatioScaler;
+import yeswecan.model.RatioScalerFactory;
+import yeswecan.model.parameters.TsTvRatioAdvanced;
+import yeswecan.phylo.GeneticStructure;
+
+/**
+ *
+ * @author Christopher Monit <c.monit.12@ucl.ac.uk>
+ */
+public class SimCAN extends SimModel {
+    
+    
+    private Tree tree;
+    private GeneticStructure genStruct;
+    private TsTvRatioAdvanced kappa;
+    private BaseFrequencies freqs;
+    private Omega[] geneOmegas;
+    private BranchScaling scaling;
+            
+    private Random rand;
+    private AlignmentBuilder siteStates;
+    private boolean printSubCounts;
+
+    
+    public SimCAN(Tree tree, GeneticStructure genStruct, double kappaValue, 
+            double[] baseFrequencyValues, double[] omegaValues, double branchScalingValue, Random rand, boolean printSubCounts){
+        
+      
+        this.tree = tree;
+        this.genStruct = genStruct;
+        this.kappa = new TsTvRatioAdvanced(kappaValue);
+        this.freqs = new BaseFrequencies(baseFrequencyValues);
+        this.scaling = new BranchScaling(branchScalingValue);
+        this.rand = rand;
+        
+        // geneOmegas[0] will be for 'no gene' regions, ie where omega=1
+        this.geneOmegas = new Omega[omegaValues.length+1];
+        this.geneOmegas[0] = new Omega(1.0); // neutral evolution
+        for (int i = 1; i < this.geneOmegas.length; i++) { // NB starting at i=1
+            this.geneOmegas[i] = new Omega(omegaValues[i-1]);
+        }
+    }
+    
+    public Alignment simulate(){
+       
+        Alignment[] sites = new Alignment[this.genStruct.getTotalLength()];
+        
+        System.out.println(genStruct.toString());
+        
+        if (this.printSubCounts){
+            System.out.println(super.subCountHeader);
+        }
+        
+        Node root = tree.getRoot();
+        
+        for (int iSite = 0; iSite < this.genStruct.getTotalLength(); iSite++) {
+            // determine evolutionary process (from site type and partition)
+            
+            int siteType = iSite % 3;
+            int[] genes = genStruct.getGenes(iSite); // the genes present in the three frames in this partition
+            Omega aOmega = this.geneOmegas[genes[0]]; // genes[0] is the gene present in frame A
+            Omega bOmega = this.geneOmegas[genes[1]]; 
+            Omega cOmega = this.geneOmegas[genes[2]]; 
+                        
+            // make model
+            
+            RatioScaler ratioScaler = RatioScalerFactory.getRatioScaler();
+            CodonAwareMatrix canQ = new CodonAwareMatrix(this.kappa, this.freqs, ratioScaler, siteType, aOmega, bOmega, cOmega, this.scaling);            
+            ProbMatrixGenerator Pgen = ProbMatrixFactory.getPGenerator(canQ);
+            
+            // simulate according to process
+            
+            int rootState = SimModel.draw(freqs.get(), rand.nextDouble());
+
+            this.siteStates = new AlignmentBuilder(this.tree.getExternalNodeCount()); // an 'alignment' for a single site, which will be populated with states by downTree.
+            
+            SubCount count = new SubCount();
+            
+            SimModel.downTree(tree, Pgen, root, rootState, count, this.siteStates, this.rand);
+            
+            if (this.printSubCounts){
+                System.out.println(iSite + Constants.OUTPUT_DELIMITER + "0" + Constants.OUTPUT_DELIMITER + iSite%3 + Constants.OUTPUT_DELIMITER + count.count); // hard coded 0 represents the alignment partition. Since there are no partitiions with HKY, every site is in partition 0
+            }
+            
+            // add this newly simulated site to the total set
+            sites[iSite] = siteStates.generateAlignment(new Nucleotides());
+        }
+        
+        Alignment allSites = new ConcatenatedAlignment(sites);
+        return allSites;
+    }
+    
+    
+    
+    
+}
