6be122f3b55a657ddfaaa08e4bf4963be7a5e544
Chris Monit <c.monit.12@ucl.ac.uk>
2016-03-07 11:21:18 +0000

    created CodonFrequencies class

---
+++ sim/CodonFrequencies.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package yeswecan.sim;
+
+/**
+ *
+ * @author Christopher Monit <c.monit.12@ucl.ac.uk>
+ */
+public class CodonFrequencies {
+    
+    
+    // take CSV instance
+    // have get methods returning freq for given codon
+    
+    
+}
