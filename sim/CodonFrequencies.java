e6d0991b234e68f163dbe7bbd29ee3afdb4f3817
Chris Monit <c.monit.12@ucl.ac.uk>
2016-03-07 15:13:43 +0000

    written class CodonFrequencies which can read in codon table and provide a given frequency if you provide the codon in int[] form

--- sim/CodonFrequencies.java
+++ sim/CodonFrequencies.java
@@ -6,6 +6,12 @@
 
 package yeswecan.sim;
 
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import yeswecan.utils.MatrixPrinter;
+
 /**
  *
  * @author Christopher Monit <c.monit.12@ucl.ac.uk>
@@ -15,6 +21,68 @@ public class CodonFrequencies {
     
     // take CSV instance
     // have get methods returning freq for given codon
+    
+    private double[][] frequencies;
+    
+    public CodonFrequencies(String frequenciesFilePath){
+        this.frequencies = new double[16][4];
+        List<String[]> textData;
+
+        try{
+            CSVReader reader = new CSVReader(new FileReader(frequenciesFilePath));
+            textData = reader.readAll();
+        }catch(IOException e){
+            System.out.println("Failed to read codon frequencies CSV");
+            textData = null;
+            e.printStackTrace();
+        }
+
+        try{
+            for (int i = 0; i < 16; i++) {
+                for (int j = 0; j < 4; j++) {
+                    this.frequencies[i][j] = Double.parseDouble(textData.get(i)[j]);
+                }
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Codon frequencies file not formatted as expected: must be in 16x4 TCAG");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        catch(NumberFormatException e){
+            System.out.println("Cannot read codon frequency element as number. Please check codon frequency file");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        
+        MatrixPrinter.PrintMatrix(frequencies, "frequencies");
+    }
+    
+    /**
+     * Assuming codons are presented out as TCAG in table,
+     * and nucleotides {T, C, A, G} = { 0, 1, 2, 3 }
+     * and a codon is made up of nucleotides { x, y, z }
+     * then
+     * i = 4x + z
+     * j = y
+     * and i and j can be used to access the codon table
+     * 
+     */
+    public double getFrequency(int[] codon){
+        int i =  4*codon[0] + codon[2];
+        int j = codon[1];
+        return this.frequencies[i][j];
+    }
+    
+    
+    
+    
+    public static void main(String[] args){
+        System.out.println("Testing CodonFreuqneices");
+        
+        CodonFrequencies cf = new CodonFrequencies(args[0]);
+        System.out.println(cf.getFrequency(new int[]{1,0,2}));
+        System.out.println("End main");
+    }
     
     
 }
