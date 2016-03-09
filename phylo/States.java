5c22b094f132861e6d7906de3aba9ccaa03d65be
Chris Monit <c.monit.12@ucl.ac.uk>
2016-03-09 13:46:51 +0000

    added comment and removed hardcoding to nuceltodie states in getMutationStates, phylo/States

--- phylo/States.java
+++ phylo/States.java
@@ -15,8 +15,13 @@ public class States {
     //determines the order in which bases will be organised. 
     //Note that PAL uses ACGT and therefore deviating from this could cause issues in the pruning algorithm
 
+    /*
+        Number of states here is number of states in total
+        ie for nucleotide there are 4 states total, 64 for codons etc
+        N = 4 means there are N-1 non-originalStates, so an array or length N-1 is returned
+    */
     public static int[] getMutationStates(int originalState, int numberOfStates){
-        int[] mutationStates = new int[]{-1, -1, -1};
+        int[] mutationStates = new int[numberOfStates-1];
         
         int mutState = 0;
         for (int jIndex = 0; jIndex < mutationStates.length; jIndex++) {
