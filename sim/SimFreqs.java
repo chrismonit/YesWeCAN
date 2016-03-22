997037879e6e734dbf1da010539fbb33fc0c761b
Chris Monit <c.monit.12@ucl.ac.uk>
2016-03-22 16:18:13 +0000

    added some initilisation code to SimFreqs (lots more to do)

--- sim/SimFreqs.java
+++ sim/SimFreqs.java
@@ -6,9 +6,13 @@
 
 package yeswecan.sim;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import pal.tree.Tree;
+import swmutsel.model.parameters.Omega;
 import yeswecan.cli.CommandArgs;
+import yeswecan.model.parameters.TsTvRatioAdvanced;
 import yeswecan.phylo.GeneticStructure;
 
 /**
@@ -20,13 +24,42 @@ public class SimFreqs {
     private int nSubs;
     private int nRepeats;
     private double equiBranchLength;
+    
     private Tree tree;
     private Random rand;
     private GeneticStructure genStruct;
+    private TsTvRatioAdvanced kappa;
+    private List<Omega> omegas;
+    private List<CodonFrequencies> codonFrequencies;
     
-    public SimFreqs(CommandArgs comaArgs){
+    
+    public SimFreqs(Tree tree, Random rand, GeneticStructure genStruct, 
+            CommandArgs comArgs){
         
-        // init
+        this.tree = tree;
+        this.genStruct = genStruct;
+        this.rand = rand;
+        
+        this.kappa = new TsTvRatioAdvanced(comArgs.kappa());
+        
+        this.omegas = new ArrayList<Omega>();
+        this.omegas.add(new Omega(1.0)); // neutral, for noncoding regions/frames
+        for (double omegaValue : comArgs.omegas()){
+            this.omegas.add( new Omega(omegaValue) ); 
+        }
+        
+        
+        
+        // need comArgs to accept one of these
+        this.codonFrequencies = new ArrayList<CodonFrequencies>();
+        
+        
+        // initialise the stuff above
+        
+        
+        FrequencySimulator simulator = new FrequencySimulator(
+            this.tree, this.rand, this.genStruct, this.kappa, this.omegas, this.codonFrequencies
+        );
         
     }
     
