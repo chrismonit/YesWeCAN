fc05735fc89b0c42bb58c7e79cad733adc75aaba
Chris Monit <c.monit.12@ucl.ac.uk>
2016-04-27 17:04:38 +0100

    the codon sum approach performed poorly in simulation tests, and the original CAN matrix method has a high false positive rate when tested with hiv codon frequencies in the simulation. We have a new approach which sums over products of codon frequencies. Implementing this now. Created class CANMatrixFreqProducts

---
+++ model/matrices/CANMatrixFreqProducts.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yeswecan.model.matrices;
+
+/**
+ *
+ * @author cmonit1
+ */
+public class CANMatrixFreqProducts {
+    
+    
+    
+    
+    
+}
