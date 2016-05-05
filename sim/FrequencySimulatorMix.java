8ed0ba1a02ff794bcb9fb63bc83b4cb8cea43cd0
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-05 14:59:58 +0100

    written constructor for FrequencySimulatorMix. Written and tested method assignGeneSiteClasses

--- sim/FrequencySimulatorMix.java
+++ sim/FrequencySimulatorMix.java
@@ -9,9 +9,11 @@ import java.util.List;
 import java.util.Random;
 import pal.tree.Tree;
 import swmutsel.model.parameters.Omega;
+import swmutsel.model.parameters.Probabilities;
 import yeswecan.model.parameters.TsTvRatioAdvanced;
 import yeswecan.phylo.CodonFrequencies;
 import yeswecan.phylo.GeneticStructure;
+import yeswecan.phylo.States;
 
 /**
  *
@@ -19,13 +21,54 @@ import yeswecan.phylo.GeneticStructure;
  */
 public class FrequencySimulatorMix extends FrequencySimulator {
     
+    private List<Probabilities> probabilities;
+    private int numSiteClasses;
+    
+    private int[][] geneSiteClasses;
+    
     public FrequencySimulatorMix(Tree tree, Random rand, GeneticStructure genStruct,
-        TsTvRatioAdvanced kappa, List<Omega> omegas, List<CodonFrequencies> codonFrequencies){
+        TsTvRatioAdvanced kappa, List<Omega> omegas, List<CodonFrequencies> codonFrequencies,
+        List<Probabilities> probabilities, int numSiteClasses
+    
+        ){
 
-        
         super(tree, rand, genStruct, kappa, omegas, codonFrequencies);
         
+        this.probabilities = probabilities;
+        this.numSiteClasses = numSiteClasses; // do we need this?
         
+        this.geneSiteClasses = assignGeneSiteClasses(
+                genStruct.getTotalLength(), genStruct.getNumberOfGenes(), 
+                rand, probabilities);
+        
+        // print the matrix so we know the true classes at each site
     }
     
+//    @Override
+//    public double computeRate(int[] quintStates, int j, int site, double nu){
+//        int[] genes = super.genStruct.getGenes(site);
+//        double product = 1.0; // multiplicative identity
+//                
+//        product *= this.kappa.getKappaIfTransition(quintStates[2], j);
+//        
+//        
+//    }
+    
+    public static int[][] assignGeneSiteClasses(int numSites, int numGenes, 
+            Random rand, List<Probabilities> probabilities){
+        
+        int[][] siteClasses = new int[numSites][numGenes];
+        
+        for (int iSite = 0; iSite < numSites; iSite++) {
+            for (int iGene = 0; iGene < numGenes; iGene++) {
+                
+                double[] probs = probabilities.get(iGene).get();
+                int siteClass = States.draw(probs, rand.nextDouble());
+                siteClasses[iSite][iGene] = siteClass;
+            }
+        }
+        
+        return siteClasses;
+    }
+
 }
