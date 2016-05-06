d87637cf31fab2f24135cc3c036415a99feaa7d9
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-06 14:14:55 +0100

    continued work on SimfreqsMix, including wriitng override of getHeader (as yet untested)

--- sim/SimFreqsMix.java
+++ sim/SimFreqsMix.java
@@ -5,8 +5,11 @@
  */
 package yeswecan.sim;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import pal.tree.Tree;
+import yeswecan.Constants;
 import yeswecan.cli.CommandArgs;
 import yeswecan.model.submodels.CANModelMixture;
 import yeswecan.phylo.GeneticStructure;
@@ -19,7 +22,9 @@ import yeswecan.run.RunCANMixture;
 public class SimFreqsMix extends SimFreqs {
         
         protected CANModelMixture canMix;
-    
+        protected CommandArgs comArgs;
+        protected int numSiteClasses;
+        
         public SimFreqsMix(Tree tree, Random rand, GeneticStructure genStruct, 
             CommandArgs comArgs){
         
@@ -31,22 +36,49 @@ public class SimFreqsMix extends SimFreqs {
             
             this.canMix = RunCANMixture.makeMixture(comArgs, comArgs.getModel());
             this.omegas = canMix.getOmegas(); // probably redundant, for completeness
+            this.comArgs = comArgs;
             
-            int numSiteClasses = RunCANMixture.numberSiteClasses(comArgs.getModel());
+            numSiteClasses = RunCANMixture.numberSiteClasses(comArgs.getModel());
             
             this.simulator = new FrequencySimulatorMix(
                     tree, rand, genStruct, kappa, this.canMix.getOmegas(),
-                    this.codonFrequencies, this.canMix.getProbabilities()
+                    this.codonFrequencies, this.canMix.getProbabilities(),
+                    numSiteClasses
             );
             
                        
-            // construct freqsimmix instance
             
             // override header method
             // override get params method
             
+            // print true stite classes thing
+            
         }
         
+        public void printSiteClasses(){
+            //TODO?
+        }
+        
+        // TEST THIS!
+        @Override
+        public String[] getHeader(){
+            ArrayList<String> columns = new ArrayList<String>();
+            Collections.addAll(columns, "model", "kappa", "nuNumRepeats", "nuNumSubs", "equiBranchLength", "allowStops");
+            
+            for (int iGene = 0; iGene < this.comArgs.getGeneNumber(); iGene++) {
+                for (int jClass = 0; jClass < this.numSiteClasses; jClass++) {
+                    columns.add(Integer.toString(iGene+1) + Constants.WITIHIN_FIELD_SEPARATOR + Constants.OMEGA_STRING + Integer.toString(jClass));
+                }
+            
+                for (int jClass = 0; jClass < this.numSiteClasses; jClass++) {
+                    columns.add(Integer.toString(iGene+1) + Constants.WITIHIN_FIELD_SEPARATOR + Constants.PROB_STRING + Integer.toString(jClass));
+                }
+            }
+            
+  
+            return columns.toArray(new String[columns.size()]);
+
+        }
         
         
         
