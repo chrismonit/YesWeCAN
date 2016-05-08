fa2617089ecae0356ee04f259f30876bd24003d8
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-08 17:57:23 +0100

    written some methods for abstract class SiteClassTable

--- io/SiteClassTable.java
+++ io/SiteClassTable.java
@@ -5,10 +5,53 @@
  */
 package yeswecan.io;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 /**
  *
  * @author cmonit1
  */
-public class SiteClassTable {
+public abstract class SiteClassTable {
+    
+    protected List<List<String>> table;
+    
+    public SiteClassTable(){
+        
+    }
+    
+    public void addDelimiterColumn(String delimiter){
+        for (List<String> stringList : this.table){
+            stringList.add(delimiter);
+        }        
+    }
+    // could have equivalent for adding rows
+    
+    public void write(String filePath, String delimiter){
+        
+        try{
+            FileWriter fileWriter = new FileWriter(filePath);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            
+            for (List<String> stringList : this.table){
+                bufferedWriter.write(String.join(delimiter, stringList));
+            }
+            
+            bufferedWriter.close();
+        
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+    
+    public void print(String lineMarker, String delimiter){
+        for (List<String> stringList : this.table){
+            System.out.println(lineMarker+delimiter+String.join(delimiter, stringList));
+        }
+    }
     
 }
