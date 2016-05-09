64bd17c1a6438800c4dedd1f7714e3a113639812
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-09 12:00:18 +0100

    written constructors for ProbSiteClassTable

--- io/ProbSiteClassTable.java
+++ io/ProbSiteClassTable.java
@@ -9,6 +9,29 @@ package yeswecan.io;
  *
  * @author cmonit1
  */
-public class ProbSiteClassTable {
+public class ProbSiteClassTable extends SiteClassTable {
+    
+    private double[][] siteClassProbs;
+    
+    public ProbSiteClassTable(double[][] siteClassProbs, int numberOfSiteClasses){
+        super();
+        this.siteClassProbs = siteClassProbs;
+        this.numberOfSiteClasses = numberOfSiteClasses;
+        this.numberOfGenes = siteClassProbs[0].length;
+        // makeTable();
+    }
+    
+    public ProbSiteClassTable(
+            double[][] siteClassProbs, int numberOfSiteClasses,
+            String majorDelimiter, String minorDelimiter
+    ){
+        this(siteClassProbs, numberOfSiteClasses);
+        this.MAJOR_DELIMITER = majorDelimiter;
+        this.MINOR_DELIMITER = minorDelimiter;
+    }
+
+    
+    
+    
     
 }
