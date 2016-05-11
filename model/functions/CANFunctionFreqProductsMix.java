af7cf4abffba62795dc8975263c14d555b2bc299
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-11 11:46:13 +0100

    written value method

--- model/functions/CANFunctionFreqProductsMix.java
+++ model/functions/CANFunctionFreqProductsMix.java
@@ -7,20 +7,26 @@ package yeswecan.model.functions;
 
 import java.util.List;
 import org.apache.commons.math3.analysis.MultivariateFunction;
+import org.apache.commons.math3.exception.MaxCountExceededException;
 import pal.datatype.CodonTable;
 import pal.tree.Tree;
 import swmutsel.model.parameters.Mapper;
 import swmutsel.model.parameters.Omega;
+import swmutsel.model.parameters.Parameter;
 import swmutsel.model.parameters.Probabilities;
 import static yeswecan.model.functions.CANFunctionFreqProducts.computeNu;
 import static yeswecan.model.functions.CANFunctionFreqProducts.createUnscaledMatrices;
 import static yeswecan.model.functions.CANFunctionFreqProducts.scaleMatrices;
+import yeswecan.model.likelihood.LikelihoodCalculator;
+import yeswecan.model.likelihood.ProbMatrixFactory;
+import yeswecan.model.likelihood.ProbMatrixGenerator;
 import yeswecan.model.matrices.CANMatrixFreqProducts;
 import yeswecan.model.submodels.CANModelFrequenciesMix;
 import yeswecan.phylo.AdvancedAlignment;
 import yeswecan.phylo.CodonFrequencies;
 import yeswecan.phylo.GeneticStructure;
 import yeswecan.phylo.States;
+import yeswecan.utils.MatrixPrinter;
 
 /**
  *
@@ -171,21 +177,68 @@ public class CANFunctionFreqProductsMix implements MultivariateFunction {
         }// iPartition
     }
     
-    @Override // TODO
+    @Override 
     public double value(double[] point) {
         
-//        
-//        Mapper.setOptimisable(this.canModel.getParameters(), point);
-//
-//        CANMatrixFreqProducts[][][][][] Q_matrices = createUnscaledMatrices(
-//                this.genStruct, this.canModel, this.codonFrequenciesArray, 
-//                this.codonTable, this.numSiteClasses);
-//        
-//        double nu = computeNu(this.genStruct, Q_matrices, this.canModel, this.numSiteClasses);
-//        scaleMatrices(this.genStruct, Q_matrices, nu);
-//        
+        Mapper.setOptimisable(this.canModel.getParameters(), point);
+
+        CANMatrixFreqProducts[][][][][] Q_matrices = createUnscaledMatrices(
+                this.genStruct, this.canModel, this.codonFrequenciesArray, 
+                this.codonTable, this.numSiteClasses);
         
-        return 0.0;
-    }
+        double nu = computeNu(this.genStruct, Q_matrices, this.canModel, this.numSiteClasses);
+        scaleMatrices(Q_matrices, this.genStruct.getNumberOfPartitions(), this.numSiteClasses, nu);
+        
+        // TODO we probably don't want to bother with this scaling in future, in which case we can just remove this line
+        scaleMatrices(Q_matrices, this.genStruct.getNumberOfPartitions(), this.numSiteClasses, this.canModel.getScaling().get());
+        
+        double totalLogL = 0.0;
+        
+        for (int iSite = 0; iSite < this.genStruct.getTotalLength(); iSite++) {
+            int partition = this.genStruct.getPartitionIndex(iSite);
+            int siteType = iSite % 3;
+            int[] genes = genStruct.getGenes(iSite); // the genes present in the three frames in this partition
+
+            double siteL = 0.0;
+            // iterate over site classes, for each gene
+            for (int iSiteClassA = 0; iSiteClassA < this.numSiteClasses; iSiteClassA++) {
+
+                for (int iSiteClassB = 0; iSiteClassB < this.numSiteClasses; iSiteClassB++) {
+
+                    for (int iSiteClassC = 0; iSiteClassC < this.numSiteClasses; iSiteClassC++) {
+
+                        CANMatrixFreqProducts Q = Q_matrices[partition][iSiteClassA][iSiteClassB][iSiteClassC][siteType];
+                        
+                        ProbMatrixGenerator P;
+                        try{
+                            P = ProbMatrixFactory.getPGenerator(Q);
+                        }
+                        catch(MaxCountExceededException e){
+                            System.out.println("Eigendecomposition failure: " + iSite);
+
+                            for (Parameter p : this.canModel.getParameters()){
+                                System.out.println(p.toString());
+                            }
+
+                            return Double.NEGATIVE_INFINITY;
+                        }
+                                                
+                        // pF = p^{F}_{i} (prob for frame F's omega being that of site class i)
+                        double pA = this.canModel.getProbability(genes[0], iSiteClassA); 
+                        double pB = this.canModel.getProbability(genes[1], iSiteClassB);
+                        double pC = this.canModel.getProbability(genes[2], iSiteClassC);
+                        
+                        double contrib = pA * pB* pC * LikelihoodCalculator.calculateSiteLikelihood(this.alignment, this.tree, iSite, P, 1.0);
+
+                        siteL += contrib;
+                    }// C
+                }// B
+            }// A
+            
+            totalLogL += Math.log(siteL);
+        }// iSite
+        
+        return totalLogL;
+    }// value
     
 }
