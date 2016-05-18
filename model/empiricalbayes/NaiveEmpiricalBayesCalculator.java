502603d10ecff6e8ae75af83308919db3fae2247
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-18 14:49:01 +0100

    methods mentioned in previous commit message were commented out in NEBcalc in that commit. now deleted

--- model/empiricalbayes/NaiveEmpiricalBayesCalculator.java
+++ model/empiricalbayes/NaiveEmpiricalBayesCalculator.java
@@ -197,69 +197,6 @@ public class NaiveEmpiricalBayesCalculator extends EmpiricalBayesCalculator {
         return probValues;
     }//computeProbValues
     
-    
-//    protected static int[] otherIntegers(int[] allInts, int intToExclude ){
-//        int[] remainingInts = new int[allInts.length-1];
-//        int adjustment = 0;
-//        for (int i = 0; i < allInts.length; i++) {
-//            if (allInts[i] == intToExclude){
-//                adjustment = 1;
-//                continue;
-//            }else{
-//                remainingInts[i-adjustment] = allInts[i];
-//            }
-//        }
-//        return remainingInts;
-//    }
-//    
-//    
-//    public static CANMatrixFreqProducts[][][][][] getQMatrices( 
-//            GeneticStructure genStruct, CANModelFrequenciesMix canModel, CodonFrequencies[] codonFrequencies,
-//            CodonTable codonTable, int numSiteClasses){
-//        
-//        CANMatrixFreqProducts[][][][][] Q_matrices = CANFunctionFreqProductsMix.createUnscaledMatrices(
-//                        genStruct, canModel, codonFrequencies, 
-//                        codonTable, numSiteClasses);
-//        
-//        double nu = CANFunctionFreqProductsMix.computeNu(genStruct, Q_matrices, canModel, numSiteClasses);
-//        
-//        CANFunctionFreqProductsMix.scaleMatrices(Q_matrices, genStruct.getNumberOfPartitions(), numSiteClasses, nu);
-//        
-//        CANFunctionFreqProductsMix.scaleMatrices(Q_matrices, genStruct.getNumberOfPartitions(), numSiteClasses, canModel.getScaling().get());
-//        
-//        return Q_matrices;
-//    }
-//    
-//    
-//    public static ProbMatrixGenerator[][][][][] createProbMatrixGenerators(CANMatrixFreqProducts[][][][][] Q_matrices){
-//        int nPartitions = Q_matrices.length;
-//        int nFrameAClasses = Q_matrices[0].length; 
-//        int nFrameBClasses = Q_matrices[0][0].length;
-//        int nFrameCClasses = Q_matrices[0][0][0].length;
-//        int nSiteTypes = Q_matrices[0][0][0][0].length;
-//        
-//        ProbMatrixGenerator[][][][][] pMatGens = new ProbMatrixGenerator[nPartitions][nFrameAClasses][nFrameBClasses][nFrameCClasses][nSiteTypes];
-//    
-//        for (int iPartition = 0; iPartition < nPartitions; iPartition++) {
-//                
-//                for (int iSiteClassA = 0; iSiteClassA < nFrameAClasses; iSiteClassA++) {
-//                    for (int iSiteClassB = 0; iSiteClassB < nFrameBClasses; iSiteClassB++) {
-//                        for (int iSiteClassC = 0; iSiteClassC < nFrameCClasses; iSiteClassC++) {
-//                            
-//                            for (int iSiteType = 0; iSiteType < nSiteTypes; iSiteType++) {
-//                            
-//                                pMatGens[iPartition][iSiteClassA][iSiteClassB][iSiteClassC][iSiteType] = 
-//                                        ProbMatrixFactory.getPGenerator( Q_matrices[iPartition][iSiteClassA][iSiteClassB][iSiteClassC][iSiteType] );
-//                            }// iSiteType
-//                    }// iSiteClassC
-//                }// iSiteClass B
-//                
-//            }// iSiteClassA
-//        }// iPartition
-//    
-//        return pMatGens;
-//    }
-        
 
     @Override
     public double[][][] getEBValues(){
