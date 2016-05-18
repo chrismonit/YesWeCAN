a0e9298f51b89c9b3c08b60a6c7606096e504afb
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-18 14:36:20 +0100

    created class CodonNaiveEmpiricalBayesCalculator, which inherits from EBcalculator. copied fields and constructor from NEBcalc with minor modification

---
+++ model/empiricalbayes/CodonNaiveEmpiricalBayesCalculator.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yeswecan.model.empiricalbayes;
+
+import pal.datatype.CodonTable;
+import pal.tree.Tree;
+import yeswecan.model.submodels.CANModelFrequenciesMix;
+import yeswecan.phylo.AdvancedAlignment;
+import yeswecan.phylo.CodonFrequencies;
+import yeswecan.phylo.GeneticStructure;
+
+/**
+ *
+ * @author cmonit1
+ */
+public class CodonNaiveEmpiricalBayesCalculator extends EmpiricalBayesCalculator {
+    
+    protected double[][][] probValues;
+    
+    protected AdvancedAlignment alignment;
+    protected Tree tree;
+    protected GeneticStructure genStruct;
+    protected CANModelFrequenciesMix canModel;
+    
+    protected CodonFrequencies[] codonFrequenciesArray;
+    protected CodonTable codonTable;
+    
+    protected int numSiteClasses;
+    
+    public CodonNaiveEmpiricalBayesCalculator(
+            AdvancedAlignment alignment, Tree tree, 
+            GeneticStructure genStruct, CANModelFrequenciesMix canModel,
+            CodonFrequencies[] codonFrequenciesArray, CodonTable codonTable,
+            int numSiteClasses
+    ){
+    
+        this.alignment = alignment;
+        this.tree = tree;
+        this.genStruct = genStruct;
+        
+        this.canModel = canModel;
+        // NB 0th omega is fixed to 1.0 for neutral evolution
+           
+        this.codonFrequenciesArray = codonFrequenciesArray;
+        this.codonTable = codonTable;
+        
+        this.numSiteClasses = numSiteClasses;
+
+        //this.probValues = computeProbValues();
+        
+    }
+    
+    @Override
+    public double[][][] getEBValues(){
+        return this.probValues;
+    }
+}
