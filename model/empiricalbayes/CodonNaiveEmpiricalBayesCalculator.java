a5f4e41e2469cc04e5ed81d3f10650f63a91bc0c
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-18 21:46:18 +0100

    written method which obtains P matrices from array, separate from large for loop used in computing marginal. This will help compute numerator as well. have not removed from for loop yet

--- model/empiricalbayes/CodonNaiveEmpiricalBayesCalculator.java
+++ model/empiricalbayes/CodonNaiveEmpiricalBayesCalculator.java
@@ -55,6 +55,70 @@ public class CodonNaiveEmpiricalBayesCalculator extends EmpiricalBayesCalculator
         
     }
     
+    protected ProbMatrixGenerator[] getCodonSiteProbMatrices(
+            ProbMatrixGenerator[][][][][] pMatGens,
+            int partition,
+            int iSiteClassV, int iSiteClassW, int iSiteClassX,
+            int iSiteClassY, int iSiteClassZ, 
+            int codonSite0Type, int codonSite1Type, int codonSite2Type
+    ){
+         int aFrameClass, bFrameClass, cFrameClass;
+                            
+        // for site C0
+        if (codonSite0Type == 0) { // C0 is alpha site
+            aFrameClass = iSiteClassV;
+            bFrameClass = iSiteClassX;
+            cFrameClass = iSiteClassY;
+        }else if (codonSite0Type == 1){ // C0 is beta site
+            aFrameClass = iSiteClassY;
+            bFrameClass = iSiteClassV;
+            cFrameClass = iSiteClassX;
+        }else{ // (vFrame == 2). C0 is gamma site
+            aFrameClass = iSiteClassX;
+            bFrameClass = iSiteClassY;
+            cFrameClass = iSiteClassV;
+        }
+
+        ProbMatrixGenerator P_c0 = pMatGens[partition][aFrameClass][bFrameClass][cFrameClass][codonSite0Type];
+
+        // site C1
+        if (codonSite1Type == 1) { // C1 is beta site
+            aFrameClass = iSiteClassV;
+            bFrameClass = iSiteClassW;
+            cFrameClass = iSiteClassY;
+        }else if (codonSite1Type == 2){ // C1 is gamma site
+            aFrameClass = iSiteClassY;
+            bFrameClass = iSiteClassV;
+            cFrameClass = iSiteClassW;
+        }else{ // codonSite1Type == 0. C1 is alpha site
+            aFrameClass = iSiteClassW;
+            bFrameClass = iSiteClassY;
+            cFrameClass = iSiteClassV;
+        }
+
+        ProbMatrixGenerator P_c1 = pMatGens[partition][aFrameClass][bFrameClass][cFrameClass][codonSite1Type];
+
+        // site C2
+        if (codonSite2Type == 2) { // C2 is gamma site
+            aFrameClass = iSiteClassV;
+            bFrameClass = iSiteClassW;
+            cFrameClass = iSiteClassZ;
+        }else if (codonSite2Type == 0){ // C2 is alpha
+            aFrameClass = iSiteClassZ;
+            bFrameClass = iSiteClassV;
+            cFrameClass = iSiteClassW;
+        }else{ // codonSite2Type == 1. //C2 is beta
+            aFrameClass = iSiteClassW;
+            bFrameClass = iSiteClassZ;
+            cFrameClass = iSiteClassV;
+        }
+
+        ProbMatrixGenerator P_c2 = pMatGens[partition][aFrameClass][bFrameClass][cFrameClass][codonSite2Type];
+        
+        return new ProbMatrixGenerator[]{ P_c0, P_c1, P_c2 };
+    }
+    
+    
     protected static int[][] geneFrames = new int[][]{
         { 0, 1, 2 },
         { 1, 2, 0 },
