18782607a49677a458ec0ea31f1d3db7fa992589
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-19 08:40:10 +0100

    changed body of for loop so it takes advantage of new method (see last commit). Still not tested.

--- model/empiricalbayes/CodonNaiveEmpiricalBayesCalculator.java
+++ model/empiricalbayes/CodonNaiveEmpiricalBayesCalculator.java
@@ -62,7 +62,7 @@ public class CodonNaiveEmpiricalBayesCalculator extends EmpiricalBayesCalculator
             int iSiteClassY, int iSiteClassZ, 
             int codonSite0Type, int codonSite1Type, int codonSite2Type
     ){
-         int aFrameClass, bFrameClass, cFrameClass;
+        int aFrameClass, bFrameClass, cFrameClass;
                             
         // for site C0
         if (codonSite0Type == 0) { // C0 is alpha site
@@ -155,66 +155,22 @@ public class CodonNaiveEmpiricalBayesCalculator extends EmpiricalBayesCalculator
                             double probProduct = 
                                     vProbs.get()[iSiteClassV] * wProbs.get()[iSiteClassW] * xProbs.get()[iSiteClassX] * yProbs.get()[iSiteClassY] * zProbs.get()[iSiteClassZ];
                                  
-                            double L = 1.0;
+                            double likelihoodProduct = 1.0;
                                                         
-                            int aFrameClass, bFrameClass, cFrameClass;
+                            ProbMatrixGenerator[] sitePMatGens = getCodonSiteProbMatrices(
+                                    pMatGens, partition,
+                                    iSiteClassV, iSiteClassW, iSiteClassX, iSiteClassY, iSiteClassZ,
+                                    codonSite0Type, codonSite1Type, codonSite2Type);
                             
-                            // for site C0
-                            if (codonSite0Type == 0) { // C0 is alpha site
-                                aFrameClass = iSiteClassV;
-                                bFrameClass = iSiteClassX;
-                                cFrameClass = iSiteClassY;
-                            }else if (codonSite0Type == 1){ // C0 is beta site
-                                aFrameClass = iSiteClassY;
-                                bFrameClass = iSiteClassV;
-                                cFrameClass = iSiteClassX;
-                            }else{ // (vFrame == 2). C0 is gamma site
-                                aFrameClass = iSiteClassX;
-                                bFrameClass = iSiteClassY;
-                                cFrameClass = iSiteClassV;
-                            }
+                            ProbMatrixGenerator P_c0 = sitePMatGens[0];
+                            ProbMatrixGenerator P_c1 = sitePMatGens[1];
+                            ProbMatrixGenerator P_c2 = sitePMatGens[2];
                             
-                            ProbMatrixGenerator P_c0 = pMatGens[partition][aFrameClass][bFrameClass][cFrameClass][codonSite0Type];
-                            L *= LikelihoodCalculator.calculateSiteLikelihood(this.alignment, this.tree, codonSite0, P_c0, 1.0);
-
+                            likelihoodProduct *= LikelihoodCalculator.calculateSiteLikelihood(this.alignment, this.tree, codonSite0, P_c0, 1.0);                           
+                            likelihoodProduct *= LikelihoodCalculator.calculateSiteLikelihood(this.alignment, this.tree, codonSite0+1, P_c1, 1.0);
+                            likelihoodProduct *= LikelihoodCalculator.calculateSiteLikelihood(this.alignment, this.tree, codonSite0+2, P_c2, 1.0);
                             
-                            // site C1
-                            if (codonSite1Type == 1) { // C1 is beta site
-                                aFrameClass = iSiteClassV;
-                                bFrameClass = iSiteClassW;
-                                cFrameClass = iSiteClassY;
-                            }else if (codonSite1Type == 2){ // C1 is gamma site
-                                aFrameClass = iSiteClassY;
-                                bFrameClass = iSiteClassV;
-                                cFrameClass = iSiteClassW;
-                            }else{ // codonSite1Type == 0. C1 is alpha site
-                                aFrameClass = iSiteClassW;
-                                bFrameClass = iSiteClassY;
-                                cFrameClass = iSiteClassV;
-                            }
-                            
-                            ProbMatrixGenerator P_c1 = pMatGens[partition][aFrameClass][bFrameClass][cFrameClass][codonSite1Type];
-                            L *= LikelihoodCalculator.calculateSiteLikelihood(this.alignment, this.tree, codonSite0+1, P_c1, 1.0);
-                            
-                            // site C2
-                            if (codonSite2Type == 2) { // C2 is gamma site
-                                aFrameClass = iSiteClassV;
-                                bFrameClass = iSiteClassW;
-                                cFrameClass = iSiteClassZ;
-                            }else if (codonSite2Type == 0){ // C2 is alpha
-                                aFrameClass = iSiteClassZ;
-                                bFrameClass = iSiteClassV;
-                                cFrameClass = iSiteClassW;
-                            }else{ // codonSite2Type == 1. //C2 is beta
-                                aFrameClass = iSiteClassW;
-                                bFrameClass = iSiteClassZ;
-                                cFrameClass = iSiteClassV;
-                            }
-                            
-                            ProbMatrixGenerator P_c2 = pMatGens[partition][aFrameClass][bFrameClass][cFrameClass][codonSite2Type];
-                            L *= LikelihoodCalculator.calculateSiteLikelihood(this.alignment, this.tree, codonSite0+2, P_c2, 1.0);
-                            
-                            double contrib = probProduct * L; 
+                            double contrib = probProduct * likelihoodProduct; 
                             sum += contrib;
                             
                         }// iSiteClassZ
