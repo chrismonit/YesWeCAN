73a63316e3e64e3f2c9ecfb9c081a8e3aaae075c
Chris Monit <c.monit.12@ucl.ac.uk>
2016-05-19 19:12:36 +0100

    writing method getCodonSites, not finsihed

--- model/empiricalbayes/OutputCodonNEB.java
+++ model/empiricalbayes/OutputCodonNEB.java
@@ -5,6 +5,8 @@
  */
 package yeswecan.model.empiricalbayes;
 
+import java.util.ArrayList;
+import java.util.List;
 import pal.datatype.CodonTable;
 import pal.tree.Tree;
 import yeswecan.model.submodels.CANModelFrequenciesMix;
@@ -28,6 +30,8 @@ public class OutputCodonNEB {
     
     protected int numSiteClasses;
     
+    protected CodonNaiveEmpiricalBayesCalculator codonNEB;
+    
     public OutputCodonNEB(
             AdvancedAlignment alignment, Tree tree, 
             GeneticStructure genStruct, CANModelFrequenciesMix canModel,
@@ -46,11 +50,48 @@ public class OutputCodonNEB {
         
         this.numSiteClasses = numSiteClasses;
         
-        CodonNaiveEmpiricalBayesCalculator codonNEB = 
+        this.codonNEB = 
                 new CodonNaiveEmpiricalBayesCalculator(
                         this.alignment, this.tree, this.genStruct, 
                         this.canModel, this.numSiteClasses);
         
     }
+    
+
+    public int[] getCodonSites(int gene){
+        
+        List<Integer> codonStartSites = new ArrayList<Integer>();
+        
+        // -2 because you can't have a codon starting right at the end of the sequence
+        for (int iSite = 0; iSite < this.genStruct.getTotalLength()-2; iSite++) {
+            
+            int partition = this.genStruct.getPartitionIndex(iSite);
+            int siteType = iSite%3;
+            
+            if (this.genStruct.containsGene(partition, gene)) {
+                
+                int frame = this.genStruct.getFrame(partition, gene);
+                
+//                int plusTwoPartition = this.genStruct.getPartitionIndex(iSite+2);
+//                boolean completeCodon = this.genStruct.
+                
+                // if this is leading site in codon. eg if frame==0 && siteType==0 then this is alpha site in frame A, 
+                // and is a first codon position by definition
+                if (frame == siteType) {
+                    codonStartSites.add(iSite);
+                }
+                                
+            }// if gene present in this partition
+            
+        }// iSite
+    
+        int[] codonStartSitesArray = new int[codonStartSites.size()];
+        for (int i = 0; i < codonStartSites.size(); i++) {
+            codonStartSitesArray[i] = codonStartSites.get(i);
+        }
+        return codonStartSitesArray;
+        
+    }// getCodonSites
+    
     
 }
