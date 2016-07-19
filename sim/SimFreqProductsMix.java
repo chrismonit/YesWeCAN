a99cc3eb3daa8f8de22134ee8dc3fe79bfd6354e
Chris Monit <c.monit.12@ucl.ac.uk>
2016-07-19 08:05:48 +0100

    written class SimFreqProductsMix but have yet to test

--- sim/SimFreqProductsMix.java
+++ sim/SimFreqProductsMix.java
@@ -6,21 +6,29 @@
 
 package yeswecan.sim;
 
+import java.util.ArrayList;
 import java.util.Random;
 import pal.alignment.Alignment;
 import pal.alignment.AlignmentBuilder;
 import pal.alignment.ConcatenatedAlignment;
+import pal.datatype.CodonTable;
+import pal.datatype.CodonTableFactory;
 import pal.datatype.Nucleotides;
 import pal.tree.Node;
 import pal.tree.Tree;
 import swmutsel.model.parameters.Omega;
 import yeswecan.Constants;
+import yeswecan.io.CommandArgs;
 import yeswecan.model.matrices.CodonAwareMatrix;
 import yeswecan.model.likelihood.ProbMatrixFactory;
 import yeswecan.model.likelihood.ProbMatrixGenerator;
 import yeswecan.model.codonawareness.RatioScaler;
 import yeswecan.model.codonawareness.RatioScalerFactory;
+import yeswecan.model.matrices.CANMatrixFreqProducts;
+import yeswecan.model.parameters.TsTvRatioAdvanced;
+import yeswecan.model.submodels.CANModelFrequenciesMix;
 import yeswecan.model.submodels.CANModelMixture;
+import yeswecan.phylo.CodonFrequencies;
 import yeswecan.phylo.GeneticStructure;
 import yeswecan.phylo.States;
 
@@ -32,20 +40,40 @@ public class SimFreqProductsMix extends SimModel { // TODO this is just SimCANMi
     
     private Tree tree;
     private GeneticStructure genStruct;
-    private CANModelMixture canMix;        
+    private CANModelFrequenciesMix canMix;
     private Random rand;
     private AlignmentBuilder siteStates;
     private boolean printSubCounts;
-
+    private CommandArgs comArgs;
+    private CodonFrequencies[] codonFrequenciesArray;
+    private TsTvRatioAdvanced kappa;
+    private CodonTable universalTable;
     
-    public SimFreqProductsMix(Tree tree, Random rand, CANModelMixture can, 
-        GeneticStructure genStruct, boolean printSubCounts){
+    
+    public SimFreqProductsMix(Tree tree, Random rand, 
+            GeneticStructure genStruct, CommandArgs comArgs, boolean printSubCounts){
       
         this.tree = tree;
         this.genStruct = genStruct;
         this.rand = rand;
-        this.canMix = can;
+        
         this.printSubCounts = printSubCounts;
+
+        this.comArgs = comArgs;
+        
+        
+        this.kappa = new TsTvRatioAdvanced(this.comArgs.kappa());
+        
+        this.codonFrequenciesArray = new CodonFrequencies[this.genStruct.getNumberOfGenes()+1]; // plus 1 for no gene case
+        codonFrequenciesArray[0] = new CodonFrequencies(); // default constructor has all freq = 1/64 for no gene case
+
+        CodonFrequencies geneFrequencies = new CodonFrequencies(comArgs.getCodonFrequencyPath()); // only allowing one set of frequencies for all genes for now at least
+        for (int i = 0; i < this.genStruct.getNumberOfGenes(); i++) {
+            this.codonFrequenciesArray[i+1] = geneFrequencies; // use reference to same instance for all genes
+        }
+            
+        this.universalTable = CodonTableFactory.createUniversalTranslator();
+        System.out.println("MODEL"+Constants.DEL+"site"+Constants.DEL+"A"+Constants.DEL+"B"+Constants.DEL+"C"); // just a header for the class and w rows
 
     }
 
@@ -54,9 +82,7 @@ public class SimFreqProductsMix extends SimModel { // TODO this is just SimCANMi
     
     public Alignment simulate(){
         Alignment[] sites = new Alignment[this.genStruct.getTotalLength()];
-        
-        
-        
+                
         Node root = tree.getRoot();
         
         for (int iSite = 0; iSite < this.genStruct.getTotalLength(); iSite++) {
@@ -69,21 +95,22 @@ public class SimFreqProductsMix extends SimModel { // TODO this is just SimCANMi
             int bFrameSiteClass = States.draw(this.canMix.getGeneProbabilities(genes[1]).get(), rand.nextDouble());
             int cFrameSiteClass = States.draw(this.canMix.getGeneProbabilities(genes[2]).get(), rand.nextDouble());
                         
-            Omega aOmega = this.canMix.getOmega(genes[0], aFrameSiteClass); // genes[0] is the gene present in frame A
-            Omega bOmega = this.canMix.getOmega(genes[1], bFrameSiteClass);  
-            Omega cOmega = this.canMix.getOmega(genes[2], cFrameSiteClass);  
+            Omega aOmega = this.canMix.getGeneAndSiteClassOmega(genes[0], aFrameSiteClass); // genes[0] is the gene present in frame A
+            Omega bOmega = this.canMix.getGeneAndSiteClassOmega(genes[1], bFrameSiteClass);  
+            Omega cOmega = this.canMix.getGeneAndSiteClassOmega(genes[2], cFrameSiteClass);  
             
-            System.out.println("MODEL"+Constants.DEL+"site"+Constants.DEL+"A"+Constants.DEL+"B"+Constants.DEL+"C"); // just a header for the class and w rows
+            Omega[] omegas = new Omega[]{ aOmega, bOmega, cOmega };
+            
             System.out.println("class"+Constants.DEL+iSite+Constants.DEL+aFrameSiteClass+Constants.DEL+bFrameSiteClass+Constants.DEL+cFrameSiteClass);
             System.out.println("w"+Constants.DEL+iSite+Constants.DEL+aOmega.get()+Constants.DEL+bOmega.get()+Constants.DEL+cOmega.get());
             
-            RatioScaler ratioScaler = RatioScalerFactory.getRatioScaler();
-            CodonAwareMatrix canQ = new CodonAwareMatrix(this.canMix.getKappa(), this.canMix.getPi(), ratioScaler, siteType, aOmega, bOmega, cOmega, this.canMix.getScaling());            
+            CANMatrixFreqProducts canQ = new CANMatrixFreqProducts(this.kappa, siteType, omegas, this.codonFrequenciesArray, this.universalTable);
+            
             ProbMatrixGenerator Pgen = ProbMatrixFactory.getPGenerator(canQ);
             
             // simulate according to process
             
-            int rootState = States.draw(this.canMix.getPi().get(), rand.nextDouble());
+            int rootState = States.draw(canQ.getBaseFrequencies().get(), rand.nextDouble());
 
             this.siteStates = new AlignmentBuilder(this.tree.getExternalNodeCount()); // an 'alignment' for a single site, which will be populated with states by downTree.
             
